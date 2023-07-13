535907f5b476bc7fb8a96a8c83c5ea8d52cab1fc
Stephen H <Sthogue16@gmail.com>
2023-07-13 14:54:51 -0500

    added HTML, CSS, and JS files

---
+++ assets/main.js
@@ -0,0 +1,10 @@
+var userFormEl = document.querySelector('#city-form');
+var cityInputEl = document.querySelector('#city');
+var fiveDayContainerEl = document.querySelector('#five-day-container');
+var currentCitySearchTerm = document.querySelector('#Current-City-search');
+
+// form event handler
+// get current city forecast
+// display current city weather
+// display 5 day forecast for the city
+
